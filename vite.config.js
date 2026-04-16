6b137e33d6af94107882c9e10b772ed4fda9d91a
rishi200201 <rishi20020107@gmail.com>
2026-04-16 10:04:12 +0530

    fixed base path

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "process.env.VITE_BASE_PATH || '/portfolio_4-26/'",
+  base: "/", // 🔥 important for Vercel
 });
